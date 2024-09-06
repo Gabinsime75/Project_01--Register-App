de663f99544b0656065fc0cba9ebbe3906f492d8
Gabin Sime <gabinsime@yahoo.com>
2024-09-06 17:07:19 -0500

    updates

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "proj01-reg-app-remote-bucket"
+    bucket         = "proj01-regapp-remote-nova-bucket"
     key            = "EKS/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "Proj01_RegApp_State-Lock"
+    region         = "us-east-1"
+    dynamodb_table = "proj01-regapp-remote-nova-table"
   }
 }
